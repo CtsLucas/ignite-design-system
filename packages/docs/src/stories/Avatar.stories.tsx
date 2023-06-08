05d11876cddab8bc02ce499886b180171f369e56
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2023-06-08 18:56:42 -0300

    feat: create Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/CtsLucas.png',
+    alt: 'Lucas Silva',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallcak: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
