555a953828095a521f9066bc5208cf3cf92c0e74
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2023-06-08 19:32:50 -0300

    feat: create TextInput component

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+import { Input, Prefix, TextInputContainter } from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <TextInputContainter>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+      <Input {...props} />
+    </TextInputContainter>
+  )
+}
